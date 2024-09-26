d6e631e02f890ceadf8d2bf8f437a2a5aa1757d2
Vinicius Sueiro <hi@vsueiro.com>
2024-09-26 14:22:18 -0400

    Add comments

--- 2024/fall/live-coding/circles/sketch.js
+++ 2024/fall/live-coding/circles/sketch.js
@@ -1,14 +1,26 @@
+// Run only once
 function setup() {
+  // Create a <canvas> element as big as the window
   createCanvas(windowWidth, windowHeight);
 
+  // Paint it pink
   background("DeepPink");
 }
 
+// Run every frame
 function draw() {
+  // Pick a fill color
   fill("Indigo");
+
+  // Pick a stroke color
   stroke("DeepPink");
 
-  let size = dist(mouseX, mouseY, pmouseX, pmouseY) * 2;
+  // Get distance from current to previous mouse position
+  let distance = dist(mouseX, mouseY, pmouseX, pmouseY);
 
+  // Make size proportional to that distance (but bigger)
+  let size = distance * 2;
+
+  // Draw a circle where the mouse is
   circle(mouseX, mouseY, size);
 }
