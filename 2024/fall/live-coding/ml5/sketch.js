1cadbd5fbaab4b29084b4feebcd884b6646fb580
Vinicius Sueiro <hi@vsueiro.com>
2024-11-07 16:55:49 -0500

    Update ml5 demo to add live-coded Bubble class

--- 2024/fall/live-coding/ml5/sketch.js
+++ 2024/fall/live-coding/ml5/sketch.js
@@ -1,8 +1,34 @@
 let video;
 let bodyPose;
+let bubbles = [];
+
+class Bubble {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 40;
+  }
+
+  render() {
+    noStroke();
+    fill("Turquoise");
+    circle(this.x, this.y, this.size);
+  }
+
+  float() {
+    // move it up
+    this.y = this.y - random(2, 4);
+    // make it smaller
+    this.size = this.size - 0.75;
+
+    if (this.size < 0) {
+      this.size = 0;
+    }
+  }
+}
 
 function preload() {
-  bodyPose = ml5.bodyPose();
+  bodyPose = ml5.bodyPose("MoveNet", { flipped: true });
 }
 
 function setup() {
@@ -13,19 +39,27 @@ function setup() {
   video.hide();
 
   bodyPose.detectStart(video, handle);
-
-  background("gray");
-  console.log(bodyPose);
 }
 
-function draw() {}
+function draw() {
+  background("Indigo");
+
+  for (let bubble of bubbles) {
+    bubble.float();
+    bubble.render();
+  }
+}
 
 function handle(poses) {
-  if (poses.length > 0) {
-    for (let pose of poses) {
-      let x = pose.nose.x;
-      let y = pose.nose.y;
-      circle(x, y, 20);
-    }
+  if (Math.random() < 0.25) {
+    return;
+  }
+
+  for (let pose of poses) {
+    let x = pose.nose.x;
+    let y = pose.nose.y;
+
+    let bubble = new Bubble(x, y);
+    bubbles.push(bubble);
   }
 }
